b5a952773ebe1c87728dd93edfb9c54994513d5f
montycolonelmustard <edgallyot@gmail.com>
2024-10-15 22:45:19 +0100

    adding to a "fake" bundle

--- build.sh
+++ build.sh
@@ -3,7 +3,14 @@
 # alr clean
 alr build
 
-if [ -e ./lib/libsami.so ]; then 
-    cp ./lib/libsami.so ./lib/sami.vst3
-fi
+pushd lib
+    if [ -d sami.vst3 ]; then 
+        rm -rf sami.vst3
+    fi
+    
+    if [ -e libsami.so ]; then 
+        mkdir -p sami.vst3/Contents/x86_64-linux
+        cp libsami.so sami.vst3/Contents/x86_64-linux/sami.so
+    fi
+popd
 
