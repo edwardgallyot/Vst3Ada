94c4cd975fe3b8508232f9e3d16201cff97fd6fd
montycolonelmustard <edgallyot@gmail.com>
2024-10-15 23:42:11 +0100

    improving build

--- build.sh
+++ build.sh
@@ -3,14 +3,18 @@
 # alr clean
 alr build
 
+echo Post Build Started.
 pushd lib
     if [ -d sami.vst3 ]; then 
+        echo Cleaning Bundle
         rm -rf sami.vst3
     fi
     
     if [ -e libsami.so ]; then 
+        echo Copying Lib to Bundle
         mkdir -p sami.vst3/Contents/x86_64-linux
         cp libsami.so sami.vst3/Contents/x86_64-linux/sami.so
     fi
 popd
+echo Post Build Completed.
 
