3e938f5a2b278578419857ed956bf0e438ac9f16
montycolonelmustard <edgallyot@gmail.com>
2024-10-15 20:50:37 +0100

    bit of config

--- build.sh
+++ build.sh
@@ -1,26 +1,7 @@
 #!/bin/zsh
 
-if [ ! -d ./obj ]; then 
-    mkdir obj
-fi
-
-if [ ! -d ./lib ]; then 
-    mkdir lib
-fi
-
-if [ -d ./lib ]; then 
-    pushd lib
-         rm -rf *
-    popd
-fi
-
-if [ -d ./obj ]; then 
-    pushd obj
-        rm -rf *
-    popd
-fi
-
-gprbuild -g -gnat2022 --no-complete-output -Pvst3 -cargs -Wl,--unresolved-symbols=ignore-in-object-files -cargs -fPIC
+# alr clean
+alr build
 
 if [ -e ./lib/libsami.so ]; then 
     cp ./lib/libsami.so ./lib/sami.vst3
